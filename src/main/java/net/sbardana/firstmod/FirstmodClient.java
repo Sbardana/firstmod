e1c17c7fb8b4a5b9b8c11182eaa0fcf1e18ec531
Sbardana <sbardana5446@gmail.com>
2024-12-28 19:45:19 +0000

    Initial commit

---
+++ src/main/java/net/sbardana/firstmod/FirstmodClient.java
@@ -0,0 +1,10 @@
+package net.sbardana.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
